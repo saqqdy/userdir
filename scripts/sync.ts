2cb8b4cfea6c3777f315965eff00f6a54057f671
saqqdy <saqqdy@qq.com>
2023-02-13 12:33:25 +0800

    init

---
+++ scripts/sync.ts
@@ -0,0 +1,3 @@
+import { execSync } from 'child_process'
+
+execSync('curl -X PUT -d "sync_upstream=true" "https://registry-direct.npmmirror.com/userdir/sync"')
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import extend from 'js-cool/lib/extend'
+
+function userdir() {
+	return {}
+}
+
+export { userdir, userdir as default }
