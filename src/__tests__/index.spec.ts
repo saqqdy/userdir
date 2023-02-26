b74de6148330d2238c365080dc67b86dd82d1492
saqqdy <saqqdy@qq.com>
2023-02-26 20:58:05 +0800

    release v1.0.0-beta.1

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,5 +1,11 @@
-import userdir from '..'
+// import { basename } from 'path'
+import userdir from '../index'
+
+// const home = process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'] || null
 
 test('defines get userdir', () => {
-	expect(userdir()).toBe('/Users/saqqdy')
+	// const user = basename(home)
+	// const match = new RegExp(user + '$')
+	// expect(userdir()).toMatch(match)
+	expect(userdir()).toBe(process.env.USERPROFILE || process.env.HOME || null)
 })
